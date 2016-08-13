8d78d48fb8614659319915481e940acd1389d1af
szd1007 <szd1007@gmail.com>
2016-08-13 13:22:26 +0800

    up

---
+++ springtest/src/main/java/com/szd1007/adamx/springtest/Instrument.java
@@ -0,0 +1,8 @@
+package com.szd1007.adamx.springtest;
+
+/**
+ * Created by zm on 2016/8/7.
+ */
+public interface Instrument {
+    void play();
+}
---
+++ springtest/src/main/java/com/szd1007/adamx/springtest/PerformerApp.java
@@ -0,0 +1,20 @@
+package com.szd1007.adamx.springtest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Created by zm on 2016/8/7.
+ */
+public class PerformerApp {
+
+
+    public static void main(String args[]){
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:performer.xml");
+        Performer performer = (Performer) ctx.getBean("duke");
+        //performer.perform();
+
+        Performer saxPer = (Performer) ctx.getBean("kenny");
+        saxPer.perform();
+    }
+}
