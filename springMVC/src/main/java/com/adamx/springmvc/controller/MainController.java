484ce18ee96dd2eabc56802ec6ead42909461cea
szd1007 <szd1007@gmail.com>
2016-08-17 17:26:41 +0800

    add springMVC

---
+++ springMVC/src/main/java/com/adamx/springmvc/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.adamx.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by shangzhidong on 2016/8/17.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/" ,method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+}
